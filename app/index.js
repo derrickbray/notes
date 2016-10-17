9a2664683c2246b4fdbe3bfa1542ea32076d4450
Derrick Bray <derrickbray@yahoo.com>
2016-10-17 10:42:37 -0500

    objects

--- app/index.js
+++ app/index.js
@@ -1,1 +1,31 @@
 import 'whatwg-fetch';
+  // practicing with prototypes
+
+const duck = 'Daffy';
+
+console.log(duck.length);
+
+console.log(duck.split(''));
+
+const h = {
+  firstName: 'Homer',
+  lastName: 'Simpson',
+  age: 39,
+  occupation: 'safety inspector',
+};
+
+function createCharacter(firstName, lastName, age, occupation) {
+  return {
+    firstName, lastName, age, occupation,
+    getFullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+}
+
+const m2 = createCharacter('marge', 'Simpson', 36, 'curently unemployed');
+
+console.log(m2.getFullName);
+console.log(m2.getFullName());
+console.log();
+console.log();
