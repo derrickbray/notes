45fa0f80e6a6815f5109ba7150abc391217f1564
Derrick Bray <derrickbray@yahoo.com>
2016-10-17 11:19:49 -0500

    constructor

--- app/index.js
+++ app/index.js
@@ -24,8 +24,30 @@ function createCharacter(firstName, lastName, age, occupation) {
 }
 
 const m2 = createCharacter('marge', 'Simpson', 36, 'curently unemployed');
+const h2 = createCharacter('homer', 'Simpson', 39, 'safety inspector');
 
 console.log(m2.getFullName);
 console.log(m2.getFullName());
-console.log();
-console.log();
+console.log(h2.getFullName);
+console.log(h2.getFullName());
+
+// the "new" keyword and constructor functions
+function Simpson(firstName, lastName, age, occupation) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.occupation = occupation;
+}
+Simpson.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+const m3 = new Simpson('marge', 'simpson', 36, 'unemployed');
+const h4 = new Simpson('homer', 'Simpson', 39, 'safety inspector');
+
+console.log(m3);
+console.log(typeof m3);
+console.log(m3 instanceof Simpson);
+console.log(m3.getFullName);
+
+// debugger;
