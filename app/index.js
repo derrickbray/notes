253bb775d6bc582d9b7c62e1511e68fb3c361177
Derrick Bray <derrickbray@yahoo.com>
2016-10-20 11:13:35 -0500

    wow

--- app/index.js
+++ app/index.js
@@ -36,6 +36,20 @@ window.setTimeout(() => {
   console.log('I should run later');
 }, 2000);
 
-console.log('i will runlater than that');
+console.log('i will run before than that');
 
 // events: wht to do in response to user
+const form = document.querySelector('.contact');
+const response = document.querySelector('.response');
+const name = document.querySelector('input[name=name]');
+const message = document.querySelector('textarea[name=message]');
+
+
+form.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+
+  response.innerText = `
+  Hello ${name.value},
+  thank you for your thoughts that: "${message.value}".
+  `;
+});
