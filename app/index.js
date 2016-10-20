ebcb8af9497a20d99325e74aa4c54b88e3e427b9
Derrick Bray <derrickbray@yahoo.com>
2016-10-20 11:37:18 -0500

    promises

--- app/index.js
+++ app/index.js
@@ -4,20 +4,16 @@ const family = [
   {
     first: 'Homer',
     age: 39,
-  },
-  {
+  }, {
     first: 'Marge',
     age: 36,
-  },
-  {
+  }, {
     first: 'Bart',
     age: 10,
-  },
-  {
+  }, {
     first: 'Lisa',
     age: 8,
-  },
-  {
+  }, {
     first: 'Maggie',
     age: 1,
   },
@@ -28,7 +24,6 @@ function printFirstName(familyMember) {
 }
 
 family.forEach(printFirstName);
-
 
 // callbacks: sending functions to other functions
 
@@ -44,7 +39,6 @@ const response = document.querySelector('.response');
 const name = document.querySelector('input[name=name]');
 const message = document.querySelector('textarea[name=message]');
 
-
 form.addEventListener('submit', (ev) => {
   ev.preventDefault();
 
@@ -52,4 +46,23 @@ form.addEventListener('submit', (ev) => {
   Hello ${name.value},
   thank you for your thoughts that: "${message.value}".
   `;
+});
+
+// promises git us a guarantee that callbackswill run
+
+const later = new Promise((resolve, reject) => {
+  setTimeout(() => {
+        // resolve('Timeout Promise');
+    reject('something went wrong');
+  }, 1000);
+});
+
+const after = document.createElement('h4');
+document.body.appendChild(after);
+
+later.then((information) => {
+  after.innerText = information + ' Changed after resolved!';
+}).catch((err) => {
+  after.innerText = 'There was an error';
+  console.log(err);
 });
