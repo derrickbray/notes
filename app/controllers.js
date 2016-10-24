36d6a0ebaa4a6eac9cf130939e63f5857af24963
Derrick Bray <derrickbray@yahoo.com>
2016-10-24 12:19:25 -0500

    still no work

--- app/controllers.js
+++ app/controllers.js
@@ -1,19 +1,23 @@
+import ResultListView from './view';
+
 export default class AppController {
   constructor(appElement) {
     this.appElement = appElement;
     this.model = [];
   }
 
-  start() {
     // grab data from api
     // when data comes back -- set up some views to show data
     // render views
     // setup a view to handle form
+  start() {
     fetch('http://tiny-tn.herokuapp.com/collections/dwb-bpm')
-    .then((res) => res.json())
-    .then((data) => {
-      this.model = data;
-      // debugger;
-    });
+        .then((res) => res.json())
+        .then((data) => {
+          this.model = data;
+
+          const resultView = new ResultListView(this.appElement.querySelector('.results-table__list'), this.model);
+          resultView.render();
+        });
   }
 }
---
+++ app/view.js
@@ -0,0 +1,27 @@
+export default class ResultListView {
+  constructor(el, model) {
+    this.el = el;
+    this.model = model;
+  }
+
+  render() {
+    // array loop our model
+    this.model.forEach((result) => {
+      const row = document.createElement('tr');
+      row.classlist.add('result');
+
+      row.innerHTML = `
+      <td class="result__time"></td>
+      <td class="result__user"></td>
+      <td class="result__bpm"></td>`;
+
+      row.querySelector('.result__time').innerText = result.time;
+      row.querySelector('.result__user').innerText = result.user;
+      row.querySelector('.result__bpm').innerText = result.bpm;
+
+      this.el.appendChild(row);
+    });
+    // create new result row
+    // append result row to the list
+  }
+}
