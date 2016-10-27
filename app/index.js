941fee23dbb74f19a88467cc1c056d6555c8b546
Derrick Bray <derrickbray@yahoo.com>
2016-10-27 10:47:18 -0500

    vue

--- app/index.js
+++ app/index.js
@@ -1,1 +1,22 @@
 import 'whatwg-fetch';
+
+import Vue from 'vue/dist/vue';
+
+const App = Vue.extend({
+
+});
+
+new App({
+  el: '.app',
+  data() {
+    return {
+      name: 'Hello from JavaScript',
+
+      pokemon: [
+        'Bulbasaur',
+        'Pikachu',
+        'Squerttle',
+      ],
+    };
+  },
+});
