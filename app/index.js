90165803388f0fb7d05a4e528363a0fcf2e303bc
Derrick Bray <derrickbray@yahoo.com>
2016-10-27 11:21:11 -0500

    vue sidebar

--- app/index.js
+++ app/index.js
@@ -9,14 +9,28 @@ const App = Vue.extend({
 new App({
   el: '.app',
   data() {
-    return {
+    const model = {
       name: 'Hello from JavaScript',
 
       pokemon: [
         'Bulbasaur',
         'Pikachu',
         'Squerttle',
+        'tom',
+        'dick',
+        'harry',
       ],
     };
+    setTimeout(() => {
+      alert('Charmander is evolving!!!');
+
+      this.pokemon = this.pokemon.map((p) => {
+        if (p === 'Pikachu') {
+          return 'Pikapoo';
+        }
+        return p;
+      });
+    }, 5000);
+    return model;
   },
 });
